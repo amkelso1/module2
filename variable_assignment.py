158e2d1af9d2aa67d42324fe25e30872f520b1f5
amkelso1 <amkelso1@dmacc.edu>
2020-09-02 09:45:26 -0500

    I dont know what to say

---
+++ variable_assignment.py
@@ -0,0 +1,16 @@
+"""
+Program: variable_assignment
+Author: Alex Kelso
+last last modified: 9/2/2020
+
+The purpose of this program is to create three variables; item, quantity, and size
+
+"""
+
+
+product_name = ("Banana")
+product_quantity = 6
+product_size = 2.4
+
+
+print(product_name, int(product_quantity), product_size)
