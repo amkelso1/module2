4b20dfb0ffeae7de08ac4ef65531dabc32ee2516
amkelso1 <amkelso1@dmacc.edu>
2020-09-08 09:55:02 -0500

    updated function to convert age, tested/passed unit test.

--- main/camper_main_input.py
+++ main/camper_main_input.py
@@ -1,13 +1,20 @@
 """
+Author: Alex Kelso
+Program: camper_age_input.py
+last date modified: 9/9/2020
 
-
-
+program that accepts an integer value for years and returns an integer representing months
 """
+import constants
 
 
-def convert_to_months(x):
-    pass
+def convert_to_months(years):
+    months = years * constants.MONTHS
+    return months
 
 
 if __name__ == '__main__':
-    print('')
+    age_in_years = input('Enter your age in years:')
+    age_in_months = convert_to_months(int(age_in_years))
+    print('{} years is {} months'.format(age_in_years, age_in_months))
+
--- test/unit_test.py
+++ test/unit_test.py
@@ -1,3 +1,12 @@
+"""
+Author: Alex Kelso
+Program: camper_age_input.py
+last date modified: 9/9/2020
+
+program that accepts an integer value for years and returns an integer representing months
+"""
+
+
 import unittest
 from main import camper_main_input
 
